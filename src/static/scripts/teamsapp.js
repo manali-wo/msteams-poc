7cb66151b0b3acf136d4fb79f91370ab62ef4e31
Webosmotic <webosmotic@Webosmotics-Mac-mini.local>
2024-10-28 17:55:17 +0530

    Updates

--- src/static/scripts/teamsapp.js
+++ src/static/scripts/teamsapp.js
@@ -4,6 +4,10 @@
   // Call the initialize API first
   microsoftTeams.app.initialize().then(function () {
     microsoftTeams.app.getContext().then(function (context) {
+      microsoftTeams.settings.setValidityState(true);
+      microsoftTeams.settings.registerOnSaveHandler(function () {
+        console.log("Saved");
+      });
       if (context?.app?.host?.name) {
         updateHubState(context.app.host.name);
       }
